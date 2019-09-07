0569fb1b3089244765cf52b1be5963816eac31a9
NagisaZj <1170863106@qq.com>
2019-09-07 15:20:50 +0800

    simple

--- rlkit/samplers/in_place.py
+++ rlkit/samplers/in_place.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from rlkit.samplers.util import rollout,SMMrollout,seedrollout, exprollout, exprollout_split
+from rlkit.samplers.util import rollout,SMMrollout,seedrollout, exprollout, exprollout_split,exprollout_splitsimple,exprolloutsimple
 from rlkit.torch.sac.policies import MakeDeterministic
 
 
@@ -95,6 +95,57 @@ class ExpInPlacePathSampler(object):
             return paths, n_steps_total
         else:
             path = exprollout_split(
+                self.env, policy, max_path_length=self.max_path_length, max_trajs=max_trajs,
+                accum_context_for_agent=accum_context_for_agent, context_agent=context_agent)
+            n_steps_total += self.max_path_length * max_trajs
+            n_trajs += max_trajs
+
+            return path, n_steps_total
+
+class ExpInPlacePathSamplerSimple(object):
+    """
+    A sampler that does not serialization for sampling. Instead, it just uses
+    the current policy and environment as-is.
+
+    WARNING: This will affect the environment! So
+    ```
+    sampler = InPlacePathSampler(env, ...)
+    sampler.obtain_samples  # this has side-effects: env will change!
+    ```
+    """
+    def __init__(self, env, policy, max_path_length,encoder):
+        self.env = env
+        self.policy = policy
+        self.encoder = encoder
+        self.max_path_length = max_path_length
+
+    def start_worker(self):
+        pass
+
+    def shutdown_worker(self):
+        pass
+
+    def obtain_samples(self, deterministic=False, max_samples=np.inf, max_trajs=np.inf, accum_context_for_agent=False, resample=1, context_agent = None,split=False):
+        """
+        Obtains samples in the environment until either we reach either max_samples transitions or
+        num_traj trajectories.
+        The resample argument specifies how often (in trajectories) the agent will resample it's context.
+        """
+        assert max_samples < np.inf or max_trajs < np.inf, "either max_samples or max_trajs must be finite"
+        policy = MakeDeterministic(self.policy) if deterministic else self.policy
+        paths = []
+        n_steps_total = 0
+        n_trajs = 0
+        if not split:
+            path = exprolloutsimple(
+            self.env, policy, max_path_length=self.max_path_length, max_trajs=max_trajs, accum_context_for_agent=accum_context_for_agent, context_agent = context_agent)
+            paths.append(path)
+            n_steps_total += len(path['observations'])
+            n_trajs += 1
+
+            return paths, n_steps_total
+        else:
+            path = exprollout_splitsimple(
                 self.env, policy, max_path_length=self.max_path_length, max_trajs=max_trajs,
                 accum_context_for_agent=accum_context_for_agent, context_agent=context_agent)
             n_steps_total += self.max_path_length * max_trajs
